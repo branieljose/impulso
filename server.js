4df8d4f2ca65ccf7bb75982c0b472e9dfbbc5f52
braniel pichardo <branielpichardo@braniels-MacBook-Pro.local>
2017-08-07 00:00:30 -0400

    socket.io removed

--- server.js
+++ server.js
@@ -67,20 +67,20 @@ app.listen(PORT, function() {
           SOCKET.IO SERVER
 =============================================================*/
 
-var io  = require('socket.io').listen(5001),
-    dl = require('delivery'),
-    fs  = require('fs');
+// var io  = require('socket.io').listen(5001),
+//     dl = require('delivery'),
+//     fs  = require('fs');
 
-io.sockets.on('connection', function(socket){
-  delivery = dl.listen(socket);
-  delivery.on('receive.success',function(file){
+// io.sockets.on('connection', function(socket){
+//   delivery = dl.listen(socket);
+//   delivery.on('receive.success',function(file){
 
-    fs.writeFile(file.name,file.buffer, function(err){
-      if(err){
-        console.log('File could not be saved.');
-      }else{
-        console.log('File saved.');
-      };
-    });
-  });
-});
+//     fs.writeFile(file.name,file.buffer, function(err){
+//       if(err){
+//         console.log('File could not be saved.');
+//       }else{
+//         console.log('File saved.');
+//       };
+//     });
+//   });
+// });
